70cd9290abc7aad55d2d1cfeecf49cc3fb329aef
MuthuKumar <muthukumar182004@gmail.com>
2026-04-01 22:03:27 +0530

    Fix header import issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from './components/header'
+import Header from './components/Header'
 import Counter from './components/count'
 const App = () => {
   return (
