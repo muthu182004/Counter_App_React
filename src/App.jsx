32300c20143b811957b2ae5053f41fa57ff7b04b
MuthuKumar <muthukumar182004@gmail.com>
2026-04-01 22:11:19 +0530

    Final fix - correct Counter import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './components/Header'
-import Counter from './components/count'
+import Counter from './components/Counter'
 const App = () => {
   return (
     <div>
